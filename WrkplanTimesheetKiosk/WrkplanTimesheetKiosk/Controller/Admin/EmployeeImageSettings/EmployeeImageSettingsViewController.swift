54ba794bff15de5132786e9a12c14eaaacd73bbc
Satabhisha Roy <satabhisha.innits@gmail.com>
2020-12-29 20:43:28 +0530

    modified

--- WrkplanTimesheetKiosk/WrkplanTimesheetKiosk/Controller/Admin/EmployeeImageSettings/EmployeeImageSettingsViewController.swift
+++ WrkplanTimesheetKiosk/WrkplanTimesheetKiosk/Controller/Admin/EmployeeImageSettings/EmployeeImageSettingsViewController.swift
@@ -213,6 +213,8 @@ extension EmployeeImageSettingsViewController: XMLParserDelegate, NSURLConnectio
         
         let text = String(format: "<?xml version='1.0' encoding='utf-8'?><soap12:Envelope xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:xsd='http://www.w3.org/2001/XMLSchema' xmlns:soap12='http://www.w3.org/2003/05/soap-envelope'><soap12:Body><ListFaces xmlns='%@/KioskService.asmx'><CorpId>%@</CorpId></ListFaces></soap12:Body></soap12:Envelope>",BASE_URL, String(describing: UserSingletonModel.sharedInstance.CorpID!))
         
+        print("corpid-=>",UserSingletonModel.sharedInstance.CorpID!)
+        
         var soapMessage = text
         let url = NSURL(string: "\(BASE_URL)/kioskservice.asmx?op=ListFaces")
         let theRequest = NSMutableURLRequest(url: url! as URL)
